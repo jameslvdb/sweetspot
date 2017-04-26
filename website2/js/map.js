8d550b92a387c431b5fe84bda0285b818208767c
Josh Satriano <jdsatch@gmail.com>
2017-04-26 15:17:41 -0500

    added bootstrap components for columns on home page

---
+++ website2/js/map.js
@@ -0,0 +1,41 @@
+//Center of the US
+var coordsOfUS = new google.maps.LatLng(37.09024, -95.712891);
+var map;
+var currentMarkers = new Array();
+
+function loadScript()
+{
+	//Create the map
+	var mapOptions = 
+	{
+	    center: coordsOfUS,
+	    zoom: 5,
+	    mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
+	var script = document.createElement('script');
+	script.type = 'text/javascript';
+	document.body.appendChild(script);
+	map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+	//Test adding markers by coordinates
+	var latitudes = [37, 37.4, 37.8];
+	var longitudes = [-95, -96, -94];
+	addMarkers(latitudes, longitudes);
+}
+
+function addMarkers(latitudes, longitudes)
+{
+	for(var i = 0; i < latitudes.length; i++)
+	{
+		var marker = new google.maps.Marker(
+			{
+				position: new google.maps.LatLng(latitudes[i], longitudes[i]),
+				map: map,
+				title: 'Hello, world!'
+			}
+		);
+		currentMarkers[i] = marker;	
+	}
+}
+
+window.onload = loadScript;
