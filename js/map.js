8ee404714428004cbe980831eb4e05b55f969e58
Laura Adams <lauraadams97@tamu.edu>
2017-04-27 22:33:14 -0500

    Added addMarkers function

--- js/map.js
+++ js/map.js
@@ -1,7 +1,8 @@
 //Center of the US
 var coordsOfUS = new google.maps.LatLng(37.09024, -95.712891);
 var map;
-var currentMarkers = new Array();
+var currentJobMarkers = new Array();
+var currentRealEstateMarkers = new Array();
 
 function loadScript()
 {
@@ -18,23 +19,82 @@ function loadScript()
 	map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
 	//Test adding markers by coordinates
-	var latitudes = [37, 37.4, 37.8];
-	var longitudes = [-95, -96, -94];
-	addMarkers(latitudes, longitudes);
+	// var latitudes = [37, 37.4, 37.8];
+	// var longitudes = [-95, -96, -94];
+	// var names = ['one', 'two', 'three'];
+	// var descriptions = ['descOne', 'descTwo','descThree'];
+	// addJobMarkers(latitudes, longitudes, names, descriptions, false);
+	// setTimeout(function() {addJobMarkers(latitudes, longitudes, names, descriptions, false);}, 2000);
+	
 }
 
-function addMarkers(latitudes, longitudes)
+function addJobMarkers(latitudes, longitudes, names, descriptions, isJob)
 {
+	var labels = 'ABCDEFGHIJ';
+
+	//Clear all old markers
+	if(isJob)
+	{
+		for(var i = 0; i < currentJobMarkers.length; i++)
+		{
+			currentJobMarkers[i].setMap(null);
+		}
+	}
+	else
+	{
+		for(var i = 0; i < currentRealEstateMarkers.length; i++)
+		{
+			currentRealEstateMarkers[i].setMap(null);
+		}
+	}
+
+
 	for(var i = 0; i < latitudes.length; i++)
 	{
 		var marker = new google.maps.Marker(
 			{
 				position: new google.maps.LatLng(latitudes[i], longitudes[i]),
 				map: map,
-				title: 'Hello, world!'
+				title: names[i],
+				draggable: true,
+     			animation: google.maps.Animation.DROP,
+				infoWindowShown: true,
+				label: labels[i]
 			}
 		);
-		currentMarkers[i] = marker;	
+
+		//Change marker depending on whether it is a job or real estate
+		if(isJob)
+		{
+			//marker.setIcon('http://maps.google.com/mapfiles/kml/paddle/blu-blank.png');
+			currentJobMarkers[i] = marker;		
+		}
+		else
+		{
+			//marker.setIcon('http://maps.google.com/mapfiles/kml/paddle/red-blank.png');	
+			currentRealEstateMarkers[i] = marker;	
+		}
+
+		//Set what the tooltip will say
+		var html = '<b>' + names[i] + '</b> <p></p>' + descriptions[i];
+
+
+		//Set up the marker to display its tooltip when clicked
+		var infowindow = new google.maps.InfoWindow
+		(
+			{
+	          content: 'nothing'
+	        }
+        );
+
+        marker.html = html;
+
+        marker.addListener('click', function() 
+	        {
+	        	infowindow.setContent(this.html);
+	        	infowindow.open(map, this);
+	        }
+        );
 	}
 }
 
